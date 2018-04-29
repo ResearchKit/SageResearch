b0420534a48fa0e726679afb8458aa5d6f45b399
Shannon Young <shannon.young@sagebase.org>
2018-04-29 01:17:03 -0700

    Comment out travis automatic build of tvOS and watchOS.
    TODO: Figure out how to setup xcode build for the tvOS and watchOS targets.

--- travis/build.sh
+++ travis/build.sh
@@ -2,14 +2,14 @@
 set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDCatalog"
-    bundle exec fastlane build scheme:"RSDTestApp"
-    bundle exec fastlane build scheme:"ResearchUI (watchOS)"
-    bundle exec fastlane build scheme:"Research (tvOS)"
+    # bundle exec fastlane build scheme:"RSDTestApp"
+    # bundle exec fastlane build scheme:"ResearchUI (watchOS)"
+    # bundle exec fastlane build scheme:"Research (tvOS)"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     bundle exec fastlane ci_archive scheme:"RSDCatalog" export_method:"app-store" 
     bundle exec fastlane ci_archive scheme:"RSDTestApp" export_method:"app-store"
-    bundle exec fastlane build scheme:"ResearchUI (watchOS)"
-    bundle exec fastlane build scheme:"Research (tvOS)"
+    # bundle exec fastlane build scheme:"ResearchUI (watchOS)"
+    # bundle exec fastlane build scheme:"Research (tvOS)"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDCatalog"
     bundle exec fastlane bump_framework scheme:"Research (iOS)" tag_name:"Research" project:"Research/Research.xcodeproj"
