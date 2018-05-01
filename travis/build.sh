73db7fbecc5ec13a051afe1183d0848b818b4093
syoung-smallwisdom <shannon.young@sagebase.org>
2018-05-01 16:51:37 -0700

    Comment out pushing RSDTest app to TestFlight

--- travis/build.sh
+++ travis/build.sh
@@ -15,9 +15,9 @@ elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag com
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDCatalog"
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDTest"
+    bundle exec fastlane bump_all
     bundle exec fastlane keychains
     bundle exec fastlane beta scheme:"RSDCatalog" export_method:"app-store" project:"RSDCatalog/RSDCatalog.xcodeproj"
-    bundle exec fastlane beta scheme:"RSDTest" export_method:"app-store" project:"RSDTest/RSDTest.xcodeproj"
-    bundle exec fastlane bump_all
+    # bundle exec fastlane beta scheme:"RSDTest" export_method:"app-store" project:"RSDTest/RSDTest.xcodeproj"
 fi
 exit $?
