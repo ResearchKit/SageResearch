23f92fdd171ce3668ea9298f4379ffd08642b5c5
syoung-smallwisdom <shannon.young@sagebase.org>
2019-04-26 14:38:27 -0700

    Update travis and move jazzy doc gen config files

--- travis/build.sh
+++ travis/build.sh
@@ -2,19 +2,18 @@
 set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDCatalog"
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDTest"
-    bundle exec fastlane build scheme:"ResearchUI (watchOS)"
+    bundle exec fastlane build scheme:"Research (watchOS)"
     bundle exec fastlane build scheme:"Research (tvOS)"
+    bundle exec fastlane build scheme:"Research (macOS)"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDCatalog"
     bundle exec fastlane keychains
     bundle exec fastlane ci_archive scheme:"RSDCatalog" export_method:"app-store" 
-    bundle exec fastlane ci_archive scheme:"RSDTest" export_method:"app-store"
-    bundle exec fastlane build scheme:"ResearchUI (watchOS)"
+    bundle exec fastlane build scheme:"Research (watchOS)"
     bundle exec fastlane build scheme:"Research (tvOS)"
+    bundle exec fastlane build scheme:"Research (macOS)"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDCatalog"
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDTest"
     bundle exec fastlane bump_all
     bundle exec fastlane keychains
     bundle exec fastlane beta scheme:"RSDCatalog" export_method:"app-store" project:"RSDCatalog/RSDCatalog.xcodeproj"
