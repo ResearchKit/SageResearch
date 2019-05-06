342c22c81dfc6afc2a1f1177cd794b644bce61d5
syoung-smallwisdom <shannon.young@sagebase.org>
2019-05-06 11:08:37 -0700

    Fix optional import of documentable

--- Research/Research/RSDDocumentable.swift
+++ Research/Research/RSDDocumentable.swift
@@ -56,6 +56,7 @@ public struct RSDDocumentCreator {
         RSDFormUIHint.self,
         RSDIdentifier.self,
         RSDKeyboardType.self,
+        RSDMotionRecorderType.self,
         RSDResultType.self,
         RSDStandardPermissionType.self,
         RSDStepType.self,
@@ -64,13 +65,6 @@ public struct RSDDocumentCreator {
         RSDTextAutocorrectionType.self,
         RSDTextSpellCheckingType.self,
         ]
-    
-    #if os(iOS)
-        let iOSEnums: [RSDDocumentableStringEnum.Type] = [
-            RSDMotionRecorderType.self,
-            ]
-        allEnums.append(contentsOf: iOSEnums)
-    #endif
         
         return allEnums
     }()
