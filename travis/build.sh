6f281c15cede8248beaa31a44e9ae608afe67807
syoung-smallwisdom <shannon.young@sagebase.org>
2019-05-06 11:32:46 -0700

    Add RSDTest back into the workspace bc looks like permissions handling changed again

--- travis/build.sh
+++ travis/build.sh
@@ -8,7 +8,8 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"RSDCatalog"
     bundle exec fastlane keychains
-    bundle exec fastlane ci_archive scheme:"RSDCatalog" export_method:"app-store" 
+    bundle exec fastlane ci_archive scheme:"RSDCatalog" export_method:"app-store"
+    bundle exec fastlane ci_archive scheme:"RSDTest" export_method:"app-store"
     bundle exec fastlane build scheme:"Research-watchOS"
     bundle exec fastlane build scheme:"Research-tvOS"
     bundle exec fastlane build scheme:"Research-macOS"
@@ -17,6 +18,6 @@ elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag co
     bundle exec fastlane bump_all
     bundle exec fastlane keychains
     bundle exec fastlane beta scheme:"RSDCatalog" export_method:"app-store" project:"RSDCatalog/RSDCatalog.xcodeproj"
-    # bundle exec fastlane beta scheme:"RSDTest" export_method:"app-store" project:"RSDTest/RSDTest.xcodeproj"
+    bundle exec fastlane beta scheme:"RSDTest" export_method:"app-store" project:"RSDTest/RSDTest.xcodeproj"
 fi
 exit $?
