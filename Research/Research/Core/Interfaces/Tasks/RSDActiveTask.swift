51e45262ad05c1fc6a063a42c6ad98e2f76f1348
Shannon Young <shannon.young@sagebase.org>
2021-03-18 10:59:17 -0700

    Fix copyright

--- Research/Research/Core/Interfaces/Tasks/RSDActiveTask.swift
+++ Research/Research/Core/Interfaces/Tasks/RSDActiveTask.swift
@@ -2,7 +2,7 @@
 //  RSDBackgroundTask.swift
 //  Research
 //
-//  Copyright © 2019 Sage Bionetworks. All rights reserved.
+//  Copyright © 2019-2021 Sage Bionetworks. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
