560f793a4081b1e9f74541f13ac4be9a2736b2ce
Shannon Young <shannon.young@sagebase.org>
2023-05-05 10:35:09 -0700

    RSDSubtaskStep is now being used by MyCogMobile

--- Research/Research/Core/Interfaces/Steps/RSDSubtaskStep.swift
+++ Research/Research/Core/Interfaces/Steps/RSDSubtaskStep.swift
@@ -8,14 +8,12 @@ import ResultModel
 import Foundation
 
 /// `RSDSubtaskStep` is a step that contains a task reference.
-@available(*,deprecated, message: "Will be deleted in a future version.")
 public protocol RSDSubtaskStep : RSDStep {
     
     /// The task for this step.
     var task: RSDTask { get }
 }
 
-@available(*,deprecated, message: "Will be deleted in a future version.")
 extension RSDSubtaskStep {
     
     public var identifier: String {
